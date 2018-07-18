0e3d9128dfde79db1abcdd0431650bb98360b84a
Ralph Samuel <theralphsamuel@gmail.com>
2018-07-17 17:29:19 -0700

    Add google cloud tools.

---
+++ src-this/google-cloud-sdk.sh
@@ -0,0 +1,7 @@
+
+#bin
+# ln -sf /Users/rsamuel/MEGASync/env/google-cloud-sdk/bin/gcloud /usr/local/bin/gcloud
+
+#completion
+source /Users/rsamuel/MEGAsync/env/google-cloud-sdk/completion.bash.inc
+soruce /Users/rsamuel/MEGAsync/env/google-cloud-sdk/path.bash.inc
