3956908fd1e7dd525eca23a151959199f92e4242
Ralph Samuel <ralph.samuel@instacart.com>
2018-09-07 12:31:40 -0700

    Updates to quick look plugins.

--- install-scripts/set-apple-settings.sh
+++ install-scripts/set-apple-settings.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 defaults write -g ApplePressAndHoldEnabled -bool false #rather than iOS style pop up for press and hold, just repeat keys.
+
+# Make text slectable in quick look
+defaults write com.apple.finder QLEnableTextSelection -bool true;
