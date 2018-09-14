c416bc95e5d7b05d60c4078e7bfb5e92bb3d31f3
Ralph Samuel <ralph.samuel@instacart.com>
2018-09-14 15:34:52 -0700

    Move utils to utils folder and add gather-screenshots.sh to it.

---
+++ src-this/utils/gather-screen-shots.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This Script takes screen shots from desktop and puts them in the screen shots folder on the desktop
+#
+#
+#by
+#ralph samuel
+#feb 1 2013
+
+
+find /Users/rsamuel/Desktop/Screen\ Shot\ * -exec mv {} /Users/rsamuel/Desktop/Screen\ Shots/ \;
