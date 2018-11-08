240251c0dd4dde33691414ed62dbc9f815c51506
Ralph Samuel <ralph.samuel@instacart.com>
2018-11-08 11:37:52 -0800

    Remove google cloud sdk from mega folder.

--- src-this/google-cloud-sdk.sh
+++ src-this/google-cloud-sdk.sh
@@ -5,3 +5,8 @@
 #completion
 source /Users/rsamuel/MEGAsync/env/google-cloud-sdk/completion.bash.inc
 source /Users/rsamuel/MEGAsync/env/google-cloud-sdk/path.bash.inc
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/rsamuel/google-cloud-sdk/path.bash.inc' ]; then . '/Users/rsamuel/google-cloud-sdk/path.bash.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/rsamuel/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/rsamuel/google-cloud-sdk/completion.bash.inc'; fi
