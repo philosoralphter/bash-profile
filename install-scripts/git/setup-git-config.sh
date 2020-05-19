2fbc7d1feae77f0ebf55db661fb108ed5af8a548
Ralph Samuel <ralph.samuel@instacart.com>
2020-05-18 22:37:12 -0700

    Add git commands: TFO, recent, trunk.

--- install-scripts/git/setup-git-config.sh
+++ install-scripts/git/setup-git-config.sh
@@ -3,6 +3,9 @@
 echo "[alias]
         history = log --pretty=format:'%C(auto) %h %ad -- (%cr) | %s%d %C(cyan)[%an]' --graph --date=short
         hist = log --pretty=format:'%C(auto)%h %ad -- %cr | %s%d %C(cyan)[%an]' --graph --date=short -15
+        recent = ! git branch --sort=committerdate | tail
+        co-trunk = !git merge-base HEAD master | xargs git checkout
+        TFO = !git ls-files -z -o --exclude-standard | xargs -0 rm -r
 [push]
         default = simple
 [color]
