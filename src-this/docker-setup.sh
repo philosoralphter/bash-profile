210565d6a39a9cc0f2d24d96a4a4acec8c9408c1
Ralph Samuel <ralph.samuel@instacart.com>
2020-05-18 22:31:13 -0700

    Turn off docker settings in bash profile.

--- src-this/docker-setup.sh
+++ src-this/docker-setup.sh
@@ -1,8 +1,8 @@
 
 #Startup if not running and Connect to docker host VM
 #docker-machine start docker-host &> /dev/null
-eval "$(docker-machine env docker-host)"
+# eval "$(docker-machine env docker-host)"
 
-#create my aliases
-alias dk='docker'
-alias dm='docker-machine'
+# #create my aliases
+# alias dk='docker'
+# alias dm='docker-machine'
