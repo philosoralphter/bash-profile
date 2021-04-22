43b1e02ad933f7fd6a906ed04625c79cc3a3b46d
Ralph Samuel <ralph.samuel@instacart.com>
2021-04-21 18:32:08 -0700

    Add be alias.

--- src-this/aliases.sh
+++ src-this/aliases.sh
@@ -20,3 +20,5 @@ alias resource='source ~/.bash_profile'
 #quck change to directories
 alias cdgo='cd ~/code/developer_mobile/go/src/github.com/philosoralphter'
 alias cdmega='cd ~/MEGASync'
+
+alias be="bundle exec"
