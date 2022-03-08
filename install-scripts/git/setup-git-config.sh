2a8f622e7964fa8af37f12f2aec130f880f8ddbd
Ralph Samuel <ralph.samuel@instacart.com>
2022-03-07 17:23:44 -0800

    Make git setup idempotent.

--- install-scripts/git/setup-git-config.sh
+++ install-scripts/git/setup-git-config.sh
@@ -1,14 +1,9 @@
+git config --global alias.history "log --pretty=format:'%C(auto) %h %ad -- (%cr) | %s%d %C(cyan)[%an]' --graph --date=short"
+git config --global alias.hist "log --pretty=format:'%C(auto)%h %ad -- %cr | %s%d %C(cyan)[%an]' --graph --date=short -15"
+git config --global alias.recent '! git branch --sort=committerdate | tail'
+git config --global alias.co-trunk '!git merge-base HEAD master | xargs git checkout'
+git config --global alias.TFO 'TFO = !git ls-files -z -o --exclude-standard | xargs -0 rm -r' # Delete untracked files
 
-
-echo "[alias]
-        history = log --pretty=format:'%C(auto) %h %ad -- (%cr) | %s%d %C(cyan)[%an]' --graph --date=short
-        hist = log --pretty=format:'%C(auto)%h %ad -- %cr | %s%d %C(cyan)[%an]' --graph --date=short -15
-        recent = ! git branch --sort=committerdate | tail
-        co-trunk = !git merge-base HEAD master | xargs git checkout
-        TFO = !git ls-files -z -o --exclude-standard | xargs -0 rm -r
-[push]
-        default = simple
-[color]
-        ui = true
-        diff = auto" >> ~/.gitconfig
- 
+git config --global push.default simple
+git config --global color.ui true
+git config --global color.diff auto
