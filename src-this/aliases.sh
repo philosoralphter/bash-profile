a9e29e6b98cb323b9e447822229e697b00fdcb56
Ralph Samuel <ralph.samuel@instacart.com>
2022-03-30 15:24:43 -0600

    Add cdcode.

--- src-this/aliases.sh
+++ src-this/aliases.sh
@@ -19,5 +19,6 @@ alias resource='source ~/.bash_profile'
 #quck change to directories
 alias cdgo='cd ~/code/developer_mobile/go/src/github.com/philosoralphter'
 alias cdmega='cd ~/MEGA'
+alias cdcode='cd ~/code'
 
 alias be="bundle exec"
