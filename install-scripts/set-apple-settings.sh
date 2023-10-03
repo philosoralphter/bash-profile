2682391068541f33f530599b58fb2a55105dc8c2
Ralph Samuel <ralph.samuel@instacart.com>
2023-10-02 17:03:24 -0700

    Slow key repeate a bit.

--- install-scripts/set-apple-settings.sh
+++ install-scripts/set-apple-settings.sh
@@ -9,7 +9,7 @@ defaults write com.apple.finder QLEnableTextSelection -bool true;
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
 
 # Keyboard key repeat settings
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 
