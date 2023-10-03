0228437122129c7705d5a0a326d31441d6352d19
Ralph Samuel <ralph.samuel@instacart.com>
2023-10-02 17:04:10 -0700

    Add some git aliases.

--- src-this/git-aliases.sh
+++ src-this/git-aliases.sh
@@ -8,4 +8,6 @@ alias hist="git hist" # own git alias for a formatted git log
 alias gpr="git pull --rebase"
 alias gco="git checkout"
 alias gcob="git checkout -b"
+alias gcot="git checkout -t"
 alias gpb="git push -u origin HEAD"
+alias gap="git add -p"
